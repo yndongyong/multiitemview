484e0a8eb32bf16393f698071123b54083889332
yndongyong <yndongyong@gmail.com>
2017-06-29 18:09:58 +0800

    simplediff

--- mutilitemview/src/main/java/com/yndongyong/widget/adapter/ItemViewProvider.java
+++ mutilitemview/src/main/java/com/yndongyong/widget/adapter/ItemViewProvider.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 /**
  * Created by dongzhiyong on 2017/5/28.
  */
@@ -12,6 +14,10 @@ public abstract class ItemViewProvider<T>  {
 
     protected SimpleViewHolder onCreateViewHolder(Context context, LayoutInflater inflater, ViewGroup parent){
         return new SimpleViewHolder(context,inflater.inflate(getLayoutId(),parent,false));
+    }
+
+    protected void onBindViewHolder(SimpleViewHolder holder, T entity, List<Object> payloads) {
+
     }
 
     public abstract int getLayoutId();
--- mutilitemview/src/main/java/com/yndongyong/widget/adapter/SimpleAdapter.java
+++ mutilitemview/src/main/java/com/yndongyong/widget/adapter/SimpleAdapter.java
@@ -1,10 +1,14 @@
 package com.yndongyong.widget.adapter;
 
 import android.content.Context;
+import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by dongzhiyong on 2017/5/29.
@@ -37,7 +41,14 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
     public void onBindViewHolder(SimpleViewHolder holder, int position) {
         int itemViewType = holder.getItemViewType();
         ItemViewProvider viewProvider = this.typePool.findViewProviderByIndex(itemViewType);
-        viewProvider.onBindViewHolder((SimpleViewHolder) holder,items.get(position));
+        viewProvider.onBindViewHolder(holder,items.get(position));
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleViewHolder holder, int position, List<Object> payloads) {
+        int itemViewType = holder.getItemViewType();
+        ItemViewProvider viewProvider = this.typePool.findViewProviderByIndex(itemViewType);
+        viewProvider.onBindViewHolder(holder,items.get(position),payloads);
     }
 
     @Override
@@ -70,6 +81,29 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleViewHolder> {
         this.items.addAll(data);
         this.notifyItemRangeChanged(size,this.items.size());
 
+    }
+    public void updateData(List<?> newData, SimpleDiff.CallBack callBack) {
+        if (newData == null || newData.isEmpty() || getItemCount()==0 || callBack==null) {
+            return;
+        }
+        Items oldList =new Items(items);
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new SimpleDiff(oldList, newData, callBack));
+        this.items.clear();
+        this.items.addAll(newData);
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            diffResult.dispatchUpdatesTo(this);
+        } else {
+            // TODO: 2017/6/29  非UI线程
+        }
+    }
+
+    public void setItems(Items data) {
+        this.items.clear();
+        this.items.addAll(data);
+    }
+
+    public Items getData() {
+        return this.items;
     }
 
 }
